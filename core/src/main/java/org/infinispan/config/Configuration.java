355ec567d4c2611075b94deb7725e3ff5a700abc
Mircea Markus <mircea.markus@gmail.com>
2012-04-10 15:35:33 +0100

    fixed NPE

--- core/src/main/java/org/infinispan/config/Configuration.java
+++ core/src/main/java/org/infinispan/config/Configuration.java
@@ -199,6 +199,8 @@ public class Configuration extends AbstractNamedCacheConfigurationBean {
       OverrideConfigurationVisitor v2 = new OverrideConfigurationVisitor();
       overrides.accept(v2);
       v1.override(v2);
+      if (newConfig == null && overrides.newConfig != null)
+         this.newConfig = overrides.newConfig;
    }
 
    @Override
@@ -1674,6 +1676,8 @@ public class Configuration extends AbstractNamedCacheConfigurationBean {
             dolly.indexing = indexing.clone();
             dolly.indexing.setConfiguration(dolly);
          }
+         if (newConfig != null)
+            dolly.newConfig = newConfig;
          dolly.fluentConfig = new FluentConfiguration(dolly);
          return dolly;
       } catch (CloneNotSupportedException e) {
