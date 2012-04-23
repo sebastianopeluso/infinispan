434866bdfcc7a8c0229d50247510b687443dce8a
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-04-23 19:33:35 +0100

    * try to fix the clear command bug

--- core/src/main/java/org/infinispan/transaction/LocalTransaction.java
+++ core/src/main/java/org/infinispan/transaction/LocalTransaction.java
@@ -210,13 +210,8 @@ public abstract class LocalTransaction extends AbstractCacheTransaction {
          super(1);
       }
 
-      private void initKeysNeededValidationIfNeeded() {
-         if (keysMissingValidation == null) {
-            keysMissingValidation = new HashSet<Object>();
-            for (WriteCommand wc : modifications) {
-               keysMissingValidation.addAll(wc.getAffectedKeys());
-            }
-         }
+      private synchronized void init(Collection<Object> affectedKeys) {
+         keysMissingValidation = new HashSet<Object>(affectedKeys);
       }
    }
 
@@ -271,7 +266,6 @@ public abstract class LocalTransaction extends AbstractCacheTransaction {
          if (prepareResult.modificationsApplied) {
             return; //already prepared
          }
-         prepareResult.initKeysNeededValidationIfNeeded();         
          prepareResult.keysMissingValidation.removeAll(keysValidated);
          if (local) {
             prepareResult.localPrepared = true;
@@ -285,15 +279,19 @@ public abstract class LocalTransaction extends AbstractCacheTransaction {
          if (prepareResult.modificationsApplied) {
             return; //already prepared
          }
-         
+
          prepareResult.result = exception;
          prepareResult.exception = true;
          prepareResult.keysMissingValidation = Collections.emptySet();
-         
+
          if (local) {
             prepareResult.localPrepared = true;
          }
          checkIfPrepared();
       }
+   }
+
+   public final void initToCollectAcks(Collection<Object> affectedKeys) {
+      prepareResult.init(affectedKeys);
    }
 }
