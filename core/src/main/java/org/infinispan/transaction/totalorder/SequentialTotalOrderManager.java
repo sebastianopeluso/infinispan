c00825f74d292aab19f91605cea8c6988389f61d
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-07-19 08:41:19 +0100

    * trying to solve the memory leak

--- core/src/main/java/org/infinispan/transaction/totalorder/SequentialTotalOrderManager.java
+++ core/src/main/java/org/infinispan/transaction/totalorder/SequentialTotalOrderManager.java
@@ -37,6 +37,11 @@ public class SequentialTotalOrderManager extends BaseTotalOrderManager {
          logProcessingFinalStatus(prepareCommand, result, exception);
          updateLocalTransaction(result, exception, prepareCommand.getGlobalTransaction());
          updateProcessingDurationStats(startTime, now());
+         if (exception) {
+            transactionTable.remoteTransactionRollback(prepareCommand.getGlobalTransaction());
+         } else {
+            transactionTable.remoteTransactionCommitted(prepareCommand.getGlobalTransaction());
+         }
       }
    }
 
