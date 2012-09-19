693dce34d8b14ac38b5bb52be65674b1f03b082f
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-09-19 17:34:50 +0100

    * refactor consistent hash

--- core/src/main/java/org/infinispan/distribution/ch/DataPlacementConsistentHash.java
+++ core/src/main/java/org/infinispan/distribution/ch/DataPlacementConsistentHash.java
@@ -5,10 +5,9 @@ import org.infinispan.dataplacement.lookup.ObjectLookup;
 import org.infinispan.remoting.transport.Address;
 
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -23,19 +22,23 @@ import java.util.Set;
 public class DataPlacementConsistentHash extends AbstractConsistentHash {
 
    private ConsistentHash defaultConsistentHash;
-   private final Map<Address, Collection<ObjectLookup>> lookUpperList;
+   private final InternalLookup[] internalLookup;
    private final ClusterSnapshot clusterSnapshot;
 
    public DataPlacementConsistentHash(ClusterSnapshot clusterSnapshot) {
       this.clusterSnapshot = clusterSnapshot;
-      lookUpperList = new HashMap<Address, Collection<ObjectLookup>>();
+      internalLookup = new InternalLookup[clusterSnapshot.size()];
    }
 
    public void addObjectLookup(Address address, Collection<ObjectLookup> objectLookup) {
       if (objectLookup == null) {
          return;
       }
-      lookUpperList.put(address, objectLookup);
+      int index = clusterSnapshot.indexOf(address);
+      if (index == -1) {
+         return;
+      }
+      internalLookup[index] = new InternalLookup(objectLookup);
    }
 
    @Override
@@ -51,31 +54,28 @@ public class DataPlacementConsistentHash extends AbstractConsistentHash {
    @Override
    public List<Address> locate(Object key, int replCount) {
       List<Address> defaultOwners = defaultConsistentHash.locate(key, replCount);
-      Collection<ObjectLookup> objectLookupCollection = lookUpperList.get(defaultOwners.get(0));
+      int primaryOwnerIndex = clusterSnapshot.indexOf(defaultOwners.get(0));
 
-      if (objectLookupCollection == null) {
+      if (primaryOwnerIndex == -1) {
          return defaultOwners;
       }
 
-      List<Address> newOwners = new LinkedList<Address>();
+      InternalLookup lookup = internalLookup[primaryOwnerIndex];
 
-      for (ObjectLookup objectLookup : objectLookupCollection) {
-         int index = objectLookup.query(key);
-         if (index != -1) {
-            newOwners.add(clusterSnapshot.get(index));
-         }
-      }
-
-      if (newOwners.size() != replCount) {
+      if (lookup == null) {
          return defaultOwners;
       }
-      return newOwners;
+
+      List<Address> newOwners = lookup.query(key);
+
+      return newOwners.size() == replCount ? newOwners : defaultOwners;
    }
 
    @Override
    public List<Integer> getHashIds(Address a) {
-      throw new UnsupportedOperationException("Not yet implemented");
+      return Collections.emptyList();
    }
+
 
    public void setDefault(ConsistentHash defaultHash) {
       defaultConsistentHash = defaultHash;
@@ -83,5 +83,25 @@ public class DataPlacementConsistentHash extends AbstractConsistentHash {
 
    public ConsistentHash getDefaultHash() {
       return defaultConsistentHash;
+   }
+
+   private class InternalLookup {
+      private final Collection<ObjectLookup> objectsLookup;
+
+      private InternalLookup(Collection<ObjectLookup> objectsLookup) {
+         this.objectsLookup = objectsLookup;
+      }
+
+      public List<Address> query(Object key) {
+         List<Address> owners = new LinkedList<Address>();
+         for (ObjectLookup lookup : objectsLookup) {
+            int owner = lookup.query(key);
+            if (owner != -1) {
+               owners.add(clusterSnapshot.get(owner));
+            }
+         }
+         return owners;
+      }
+
    }
 }
