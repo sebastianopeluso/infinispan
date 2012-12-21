64ea6ec85f5c5f3404f5970887a95efe15febdfc
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-12-21 15:42:53 +0000

    Configuration changes and promove version

--- core/src/main/java/org/infinispan/configuration/parsing/Parser52.java
+++ core/src/main/java/org/infinispan/configuration/parsing/Parser52.java
@@ -242,6 +242,7 @@ public class Parser52 implements ConfigurationParser<ConfigurationBuilderHolder>
                throw ParseUtils.unexpectedAttribute(reader, i);
          }
       }
+      ParseUtils.requireNoContent(reader);
    }
    
    private void parseModules(XMLExtendedStreamReader reader, ConfigurationBuilderHolder holder)
